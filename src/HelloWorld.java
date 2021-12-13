413902441849b892830e50a1039642afcc504873
rr160 <ramanisankar.r@gmail.com>
2021-12-13 17:23:16 +0100

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+// 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello world!");
+
+	class car_1 {
+		static void hello() {
+			
+		}		
+	}
+
+	
+	}
+
+}
